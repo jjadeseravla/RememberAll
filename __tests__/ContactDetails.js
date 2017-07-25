4b6b7fb00190c3eca8afc1c9a9965398b7d6cea2
Kynosaur <kye.bracey@gmail.com>
2017-07-25 17:05:19 +0100

    Updates ContactDetails Jest tests to pass in fake navigator prop on rendering

--- __tests__/ContactDetails.js
+++ __tests__/ContactDetails.js
@@ -6,16 +6,18 @@ import ContactDetails from '../gen/components/ContactDetails.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const navigationFake = {state: {params: {contact: {name: 'fakecontact', tags: ['faketag']}}}}
+
 describe('ContactDetails', () => {
 
   it('renders correctly', () => {
     const tree = renderer.create(
-      <ContactDetails />
+      <ContactDetails navigation={navigationFake} />
     );
   });
 
   it('Renders details of a contact', () => {
-    const jsontree = renderer.create(<ContactDetails />).toJSON();
+    const jsontree = renderer.create(<ContactDetails navigation={navigationFake}/>).toJSON();
     expect(jsontree.children[0].type).to.equal('Text');
   });
 });

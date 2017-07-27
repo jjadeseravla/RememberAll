772f8394c6df1df3f1811ade84b59830588e76fc
Ľuboš Michalič <lnmichalic@gmail.com>
2017-07-27 10:40:56 +0100

    Revert "Prevent duplicate tags"

--- gen/components/ContactDetails.js
+++ gen/components/ContactDetails.js
@@ -8,21 +8,12 @@ class ContactDetails extends React.Component {
     super(props)
     this.state = {contact: this.props.navigation.state.params.contact}
   }
-
-  listTags(contact) {
-    const tagsList = []
-    for (let index = 0, length = contact.tags.length; index<length; index++) {
-      tagsList.push(contact.tags[index].name + ' ')
-     }
-    return tagsList
-  };
-
   render() {
     return (
       <View ref={this.props.generateTestHook(`ContactDetails.${this.state.contact.name}`)}>
         <Text>
           { this.state.contact.name }
-          { this.listTags(this.state.contact) }
+          { this.state.contact.tags[0].name }
         </Text>
       </View>
     )

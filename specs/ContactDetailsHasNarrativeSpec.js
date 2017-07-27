1abdf65a906202efc114f7aa8eab915636ae6181
Kynosaur <kye.bracey@gmail.com>
2017-07-27 15:36:41 +0100

    Fixes Cavy test with duplicate name

--- specs/ContactDetailsHasNarrativeSpec.js
+++ specs/ContactDetailsHasNarrativeSpec.js
@@ -3,17 +3,15 @@ export default function(spec) {
   spec.describe('Contact details have a section that', function() {
 
     spec.it('shows the narrative', async function() {
-      await spec.fillIn('NewContactForm.NameInput', 'Kyle');
+      await spec.fillIn('NewContactForm.NameInput', 'Kye');
       await spec.fillIn('NewContactForm.RoleInput', 'WebDev');
       await spec.fillIn('NewContactForm.OrganisationInput', 'Makers Academy');
       await spec.fillIn('NewContactForm.ContextInput', 'Ruby Conf');
       await spec.press('NewContactForm.Button');
       await spec.pause(50);
-      await spec.press('Contact.Kyle')
-      await spec.exists('ContactDetails.Kyle')
-      await spec.exists('ContactDetails.Kyle.Narrative')
+      await spec.press('Contact.Kye')
+      await spec.exists('ContactDetails.Kye')
+      await spec.exists('ContactDetails.Kye.Narrative')
     });
-
-
   });
 }
